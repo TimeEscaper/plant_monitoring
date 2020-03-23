d31b588e4f990c46c02fcd72da68edbe7dcb3421
TimeEscaper <rentgeny05@gmail.com>
2020-03-23 04:32:47 +0300

    Old files removed

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-export PLANT_MONITOR_DIR=$(pwd)
-cat $PLANT_MONITOR_DIR/crontab | tee -a /etc/crontab
-
--- scripts/blur_util.py
+++
@@ -1,7 +0,0 @@
-import cv2
-# See https://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
-
-def is_image_blurred(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    fm = cv2.Laplacian(gray, cv2.CV_64F).var()
-    return fm < 95.0
